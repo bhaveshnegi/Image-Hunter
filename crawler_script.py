4316b5f22a690a9e933bd113b0f13fdeb27f6fa9
Bhavesh Negi <bhaveshnegi98@gmail.com>
2025-09-04 17:22:45 +0530

    Restructure the folder

--- crawler_script.py
+++ crawler_script.py
@@ -1,4 +1,12 @@
 from icrawler.builtin import GoogleImageCrawler
 
-crawler = GoogleImageCrawler(storage={'root_dir': '0-images'})
-crawler.crawl(keyword='0', max_num=1000)
+def download_images(keyword, max_num=600, folder="images"):
+    google_crawler = GoogleImageCrawler(storage={'root_dir': folder})
+    google_crawler.crawl(keyword=keyword, max_num=max_num)
+
+if __name__ == "__main__":
+    # 🔹 Change keyword here for your product
+    download_images(keyword="Nike shoes", max_num=50, folder="images/nike")
+    # Example for multiple products:
+    # download_images(keyword="Adidas shoes", max_num=600, folder="images/adidas")
+    # download_images(keyword="Puma shoes", max_num=600, folder="images/puma")
